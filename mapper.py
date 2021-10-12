d50490e14e9ac431626e253b4ccba3091c0d34c7
Karina Johansson <kahjohansson@gmail.com>
2021-10-12 19:00:46 -0300

    update mapper

--- mapper.py
+++ mapper.py
@@ -2,6 +2,13 @@
 import sys
 
 
+def read_input(file):
+    file = file.readlines()
+    for line in file:
+        # split the line into words
+        yield line.strip()
+
+
 def main(k=9, separator='\t'):
     """ Maps all of the k-mers from input
 
@@ -9,10 +16,11 @@ def main(k=9, separator='\t'):
     ----------
     k : size of the subsequence of nucleotides, the k-mer
     separator : ouput separator, that is placed between k-mer and value count"""
-    string = sys.stdin.readlines()[0].strip()
-    n = len(string)
-    for i in range(n-k+1):
-        print (f'{string[i:i+k]}{separator}1')
+    data = read_input(sys.stdin)
+    for string in data:
+        n = len(string)
+        for i in range(n-k+1):
+            print (f'{string[i:i+k]}{separator}1')
 
 
 if __name__ == "__main__":
